1ae040140427fc825f68f3cf009154f0b6ae38aa
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-07-25 09:37:31 +0100

    adding new grid background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Background from './components/Background';
 import Dropdown from './components/Dropdown';
 import './styles/_app.scss';
 
 export default () => {
   return (
     <div>
+      <Background />
       <Dropdown />
     </div>
   );
---
+++ src/components/Background.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import a from '../images/letters/0.png';
+import l from '../images/letters/1.png';
+import e from '../images/letters/2.png';
+import x from '../images/letters/3.png';
+import b from '../images/letters/4.png';
+import o from '../images/letters/5.png';
+import t from '../images/letters/6.png';
+import tt from '../images/letters/7.png';
+import ee from '../images/letters/8.png';
+import n from '../images/letters/9.png';
+import bb from '../images/letters/10.png';
+import eee from '../images/letters/11.png';
+import r from '../images/letters/12.png';
+import g from '../images/letters/13.png';
+import '../styles/_app.scss';
+
+const Background = () => {
+  const letters = [
+    a, l, e, x, b, o, t, tt, ee, n, bb, eee, r, g
+  ];
+
+  const renderedLetters = letters.map((letter) => {
+    return (
+      <div className="letter" style={{ "text-align": "center "}}>
+        <img src={letter} alt="background" />
+      </div>
+    );
+  })
+
+  return (
+    <div className="background-container">
+     {renderedLetters}
+    </div>
+  );
+};
+
+export default Background;
